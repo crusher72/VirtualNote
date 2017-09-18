a643f4d4fac19d230dc90fe161a57bdfbd5df78a
crusher72 <21303200+crusher72@users.noreply.github.com>
2017-09-17 23:15:20 -0700

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,7 +106,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
- // "virtualnote.com:8080",
+    "localhost:7227",
  // "your_seed_ip2.com:8080",
 };
 
