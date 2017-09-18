15e41598933ef1825cd566220381221335e469e5
crusher72 <21303200+crusher72@users.noreply.github.com>
2017-09-17 22:50:11 -0700

    Update P2pNetworks.h

--- src/P2p/P2pNetworks.h
+++ src/P2p/P2pNetworks.h
@@ -6,5 +6,5 @@
 
 namespace CryptoNote
 {
-  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 1x43, 1x52, 2x57, 52x50, 1x54, 1x4f, 2x4e, 1x4f, 7x54, 7x45, 2x00, 1x12, 0x10, 1x11, 3x01, 1x11} };
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 1x43, 1x52, 2x57, 52x50, 1x54, 1x4f, 2x4e, 1x4f, 7x54, 7x45, 2x00, 1x12, 4x10, 1x11, 3x01, 1x11} };
 }
